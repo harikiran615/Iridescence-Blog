5dd379ab73661f3de022c216d5ea4621e6186ef5
Harikiran <lodeharikiran50612@gmail.com>
2024-09-01 22:37:54 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
 
-export default function componentName() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About'; 
+import Signin from './pages/Signin'; 
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'; 
+import Projects from './pages/Projects'; 
+
+
+export default function App() {
   return (
-    <h1 >hello</h1>
-  );
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+   
+  )
 }
 
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            projecrs
+        </div>
+    );
+};
+
+export default Projects;
